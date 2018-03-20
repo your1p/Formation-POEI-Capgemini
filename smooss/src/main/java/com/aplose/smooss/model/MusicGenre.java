8e537bfdaf31738b9ca7df760e01fd3b4610f2c8
formation <formation@formation-Latitude-5480>
2018-03-20 11:00:24 +0100

    MusicGenre Class (enum) created

---
+++ smooss/src/main/java/com/aplose/smooss/model/MusicGenre.java
@@ -0,0 +1,23 @@
+package com.aplose.smooss.model;
+
+public enum MusicGenre {
+	Blues,
+	Chansons_Françaises,
+	Classical,
+	Electro,
+	French_Variety,
+	HardRock,
+	Hip_Hop,
+	Jazz,
+	Latino,
+	Metal,
+	Pop,
+	Punk,
+	Reggae,
+	RnB,
+	Rock,
+	Techno,
+	World,
+	Other_unclassified;
+	
+}
