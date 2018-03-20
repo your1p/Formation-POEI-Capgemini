2674f519e0819d75c31a74bf5fa2cf0ea634e9ac
formation <formation@formation-Latitude-5480>
2018-03-20 20:15:46 +0100

    Update of Picture class.

--- smooss/src/main/java/com/aplose/smooss/model/Picture.java
+++ smooss/src/main/java/com/aplose/smooss/model/Picture.java
@@ -1,5 +1,32 @@
 package com.aplose.smooss.model;
 
-public class Picture {
+import java.awt.Image;
+import java.util.List;
 
+/**
+ * Picture Class, create a picture.
+ * @author Martin M
+ *
+ */
+
+public class Picture
+{
+
+	private Image picture = null;
+	private String name = null;
+	private String description = null;
+	private User author = null;
+	//List<Comment> comments = new List<Comment>();
+
+	Picture(Image picture)
+	{
+		this.picture = picture;
+	}
+	
+	Picture(Image picture, String name)
+	{
+		this.picture = picture;
+		this.name = name;
+	}
+	
 }
