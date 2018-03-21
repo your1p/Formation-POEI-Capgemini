2e827483c1bc09f20dae91761adcc1849122ec8e
Youlie <julie.lefebvre26@outlook.fr>
2018-03-21 16:59:06 +0100

    Commit origin class Event by ju

--- smooss/src/main/java/com/aplose/smooss/model/Event.java
+++ smooss/src/main/java/com/aplose/smooss/model/Event.java
@@ -1,7 +1,18 @@
 package com.aplose.smooss.model;
 
-public class Event {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Event extends User{
 
 
-//zzzzhhs
+	//private Admin User ;
+	private String name = null;
+	private String description = null;
+	private String localisation = null;
+	private Date start = null;
+	private Date end = null;
+	
+	List<Module> modules = new ArrayList<Module>();
 }
