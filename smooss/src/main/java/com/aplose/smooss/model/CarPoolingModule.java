ce444d06c40c67b8e8c4ee8a7a10406783fb23cf
formation <formation@Cynthia-Latitude-5480>
2018-03-21 17:04:46 +0100

    Add hierarchy and carpooling list

--- smooss/src/main/java/com/aplose/smooss/model/CarPoolingModule.java
+++ smooss/src/main/java/com/aplose/smooss/model/CarPoolingModule.java
@@ -1,6 +1,16 @@
 package com.aplose.smooss.model;
 
-public class CarPoolingModule {
+import java.util.List;
 
+public class CarPoolingModule extends Module {
+
+	private List<String> carPooling;
+
+	public List<String> getCarPooling() {
+		return carPooling;
+	}
+	
+	
+	
+	
 }
-
