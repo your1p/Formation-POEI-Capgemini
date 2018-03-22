e2ecc8b1b1fbcc3eb49738bb4bc4f65bc18ec9ff
formation <formation@formation-Latitude-5480>
2018-03-22 10:08:12 +0100

    getter and setter put in "BringModel"

--- smooss/src/main/java/com/aplose/smooss/model/BringModule.java
+++ smooss/src/main/java/com/aplose/smooss/model/BringModule.java
@@ -1,11 +1,35 @@
 package com.aplose.smooss.model;
 
 public class BringModule extends Module {
-
-	private TypeOfBring type;
 	
-	public TypeOfBring type() {
+	private TypeOfBring type;
+	public TypeOfBring getType() {
 		return type;
 	}
+	
+	private String what;
+	public String getWhat() {
+		return what;
+	}
+	public void setWhat (String what) {
+		this.what = what;
+	}
+
+	private String howMany;
+	public String getHowMany() {
+		return howMany;
+	}
+	public void setHowMany (String howmany) {
+		this.howMany = howmany;
+	}
+	
+	private String who;
+	public String getWho() {
+		return who;
+	}
+	public void setWho(String who) {
+		this.who = who;
+	}
+	
 	
 }
