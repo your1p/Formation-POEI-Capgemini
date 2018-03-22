ffbbce4fee0537d2502d18b350f353f32f85d404
vallsa <vallsa@vallsa-Latitude-5480>
2018-03-22 10:26:57 +0100

    Modification PicturesModules

--- smooss/src/main/java/com/aplose/smooss/model/PicturesModule.java
+++ smooss/src/main/java/com/aplose/smooss/model/PicturesModule.java
@@ -4,30 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PicturesModule extends Module {
-			
-		private List<PicturesModule> picture; 
-	
-		public List<PicturesModule> asList(PicturesModule[] picture) {
-			
-			List<PicturesModule> result = new ArrayList<PicturesModule>();
-			for (PicturesModule pict : result) {
-				
-				result.add(pict);
-				
-			}
-			return result;
-			
-			
+
+	private List<PicturesModule> picture;
+
+	public List<PicturesModule> asList(PicturesModule[] picture) {
+
+		List<PicturesModule> result = new ArrayList<PicturesModule>();
+		for (PicturesModule pict : result) {
+
+			result.add(pict);
+
 		}
+		return result;
+
+	}
 
 		public List<PicturesModule> getPicture() {
 			return picture;
 		}
 
-		public void setPicture(List<PicturesModule> picture) {
-			this.picture = picture;
-		}
-		
-	
+//		public void setPicture(List<PicturesModule> picture) {
+//			this.picture = picture;
+//		}
+
 }
- 
