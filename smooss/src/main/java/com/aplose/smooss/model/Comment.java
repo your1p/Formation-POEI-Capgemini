d4c32c8812fb3245fefa785aaad2d400e506e2d8
formation <formation@formation-Latitude-5480>
2018-03-22 13:09:29 +0100

    add Get/Set in Comment and List in CommentTest

--- smooss/src/main/java/com/aplose/smooss/model/Comment.java
+++ smooss/src/main/java/com/aplose/smooss/model/Comment.java
@@ -1,32 +1,43 @@
 package com.aplose.smooss.model;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Scanner;
 
 public class Comment {
 	private String comment;
-	private Instant createInstant; 
+	private Instant date;
+	private Instant hour;
 	private User author;
-	
+
 	public String getComment() {
 		return comment;
 	}
+
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-	public Instant getCreateInstant() {
-		return createInstant;
+
+	public Instant getDate() {
+		return date;
 	}
-	public void setCreateInstant(Instant createInstant) {
-		this.createInstant = createInstant;
+
+	public void setDate(Instant date) {
+		this.date = date;
 	}
+
+	public Instant getHour() {
+		return date;
+	}
+
+	public void setHour(Instant hour) {
+		this.hour = hour;
+	}
+
 	public User getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(User author) {
 		this.author = author;
 	}
-	
+
 }
