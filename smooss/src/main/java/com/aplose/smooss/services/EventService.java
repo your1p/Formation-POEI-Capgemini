28a704c8c8e919619a7370bccd9af4469c5eeba4
TwiKeySen <tomas.gonzalez.vega@gmail.com>
2018-04-11 11:42:10 +0200

    Added EventService(methode getInstance, create, read )

--- smooss/src/main/java/com/aplose/smooss/services/EventService.java
+++ smooss/src/main/java/com/aplose/smooss/services/EventService.java
@@ -1,5 +1,30 @@
 package com.aplose.smooss.services;
 
+import com.aplose.smooss.model.Event;
+
 public class EventService {
 
+	private static EventService INSTANCE;
+	
+	private EventService() { }
+	
+	public static EventService getInstance() {
+		if ( INSTANCE == null ) {
+			INSTANCE = new EventService();
+		}
+		
+		return INSTANCE;
+	}
+	
+	public void create(Event evt) {
+		JPASingleton.getInstance().getEntityManager().getTransaction().begin();
+		JPASingleton.getInstance().getEntityManager().persist(evt);
+		JPASingleton.getInstance().getEntityManager().getTransaction().commit();
+	}
+	
+	public Event read(long id) {
+		Event evt = JPASingleton.getInstance().getEntityManager().find(Event.class, id);
+		return evt;
+	}
 }
+ 
