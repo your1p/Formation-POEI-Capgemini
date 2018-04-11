523ca618360be389df2ecd4ee44020f0d52f2aa7
formation <formation@formation-Latitude-5480>
2018-04-11 12:26:09 +0200

    User class created with JPA relations

--- smooss/src/main/java/com/aplose/smooss/model/User.java
+++ smooss/src/main/java/com/aplose/smooss/model/User.java
@@ -1,5 +1,33 @@
 package com.aplose.smooss.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private long login;
+	private long password;
+	
+	//Getters & Setters
+	public long getLogin() {
+		return login;
+	}
+	public void setLogin(long login) {
+		this.login = login;
+	}
+	public long getPassword() {
+		return password;
+	}
+	public void setPassword(long password) {
+		this.password = password;
+	}
+	
+	
+	
 }
