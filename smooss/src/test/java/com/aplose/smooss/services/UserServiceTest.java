d7d6a426f434f46b945cea26e47e214880996de2
formation <formation@formation-Latitude-5480>
2018-04-11 15:09:47 +0200

    Le dossier qui contient persistence.xml doit s'appeler META-INF et non MAIN-INF

--- smooss/src/test/java/com/aplose/smooss/services/UserServiceTest.java
+++ smooss/src/test/java/com/aplose/smooss/services/UserServiceTest.java
@@ -1,9 +1,26 @@
 package com.aplose.smooss.services;
 
+import com.aplose.smooss.model.User;
+
 import junit.framework.TestCase;
 
 public class UserServiceTest extends TestCase {
 	
-	//private User 
+	public void testFindByLoginAndPassword() {
+		
+		UserService us = UserService.getInstance();
+		User u = new User();
+		u.setLogin("Gustave");
+		u.setPassword("password");
+		us.create(u);
+		
+		User u2 = us.findByLoginAndPassword("Gustave", "password");
+		assertNotNull(u2);
+		assertEquals(u.getId(), u2.getId());
+		
+		User u3 = us.findByLoginAndPassword("Gustave", "badpassword");
+		assertNull(u3);
+		
+	}
 
 }
