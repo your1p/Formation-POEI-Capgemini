ea002ad2219b1a673c3bd0ec8f2e89d9b574cda2
vallsa <vallsa@vallsa-Latitude-5480>
2018-04-11 11:59:48 +0200

    Write class in InviteService(INSTANCE, method create and read)

--- smooss/src/main/java/com/aplose/smooss/services/InviteService.java
+++ smooss/src/main/java/com/aplose/smooss/services/InviteService.java
@@ -1,5 +1,27 @@
 package com.aplose.smooss.services;
 
+import com.aplose.smooss.model.Invite;
+
 public class InviteService {
+	
+	private static InviteService INSTANCE;
+	
+	private InviteService () {}
+	
+	public static InviteService getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new InviteService();
+		}
+		return INSTANCE;
+	}
+	public void create(Invite ivt) {
+		JPASingleton.getInstance().getEntityManager().getTransaction().begin();
+		JPASingleton.getInstance().getEntityManager().persist(ivt);
+		JPASingleton.getInstance().getEntityManager().getTransaction().commit();
+	}
+	public Invite read(long id) {
+		Invite ivt = JPASingleton.getInstance().getEntityManager().find(Invite.class, id);
+		return ivt;
+	}
 
 }
