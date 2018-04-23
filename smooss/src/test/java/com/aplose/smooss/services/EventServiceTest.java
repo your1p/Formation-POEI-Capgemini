65499c86830a3cb5edffafe95c0f250eef25d50e
xhackax47 <xhackax47@gmail.com>
2018-04-23 09:15:29 +0200

    Throw email exception pour us.create(u)

--- smooss/src/test/java/com/aplose/smooss/services/EventServiceTest.java
+++ smooss/src/test/java/com/aplose/smooss/services/EventServiceTest.java
@@ -3,6 +3,7 @@ package com.aplose.smooss.services;
 import java.time.Instant;
 import java.util.List;
 
+import com.aplose.smooss.exception.EmailException;
 import com.aplose.smooss.model.Event;
 import com.aplose.smooss.model.User;
 
@@ -10,7 +11,7 @@ import junit.framework.TestCase;
 
 public class EventServiceTest extends TestCase {
 	
-	public void testFindEventsByUser() {
+	public void testFindEventsByUser() throws EmailException {
 		
 		UserService us = UserService.getInstance();
 		User u = new User();
